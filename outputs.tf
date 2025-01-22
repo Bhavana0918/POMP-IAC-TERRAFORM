e4a66ab4392247023640946ef8144ef07604ea32
Bhavana.D <bhavanard21@gmail.com>
2025-01-22 17:28:50 +0530

    add

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,8 @@ output "amplify_app_url" {
 output "lambda_function_arn" {
   value = module.lambda.lambda_function_arn
 }
+
+output "ssm_api_gateway_value" {
+  sensitive = true
+  value = module.ssm_api_gateway.ssm_api_gateway_url
+}
